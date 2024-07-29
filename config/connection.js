f48f4e65051b999577d7af6603ad93941ecd2935
Ashley Wright <ashleywright@iMac.local>
2024-07-29 16:11:21 -0400

    finished adding all of my functions to add department, role and employee as well as view department, role and employee

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,22 @@ const client = new Client({
 
 client.connect();
 
+const addDepartment = (name) => {
+    return client.query('INSERT INTO departments (name) VALUES ($1) RETURNING *', [name]);
+};
+
+const addRole = (title, salary, departmentId) => {
+    return client.query('INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *', [title, salary, departmentId]);
+};
+
+const addEmployee = (firstName, lastName, roleId, managerId) => {
+    return client.query('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *', [firstName, lastName, roleId, managerId]);
+};
+
+const updateEmployeeRole = (employeeId, roleId) => {
+    return client.query('UPDATE employees SET role_id = $1 WHERE id = $2', [roleId, employeeId]);
+};
+
 const viewAllDepartments = () => {
     return client.query('SELECT * FROM departments');
 };
@@ -33,11 +49,12 @@ const viewAllEmployees = () => {
     
 };
 
-const addDepartment = (name) => {
-    return client.query('INSERT INTO departments (name) VALUES ($1) RETURNING *', [name]);
+module.exports = {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
 };
-
-const addRole = (title, salary, departmentId) => {
-    return client.query('INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *', [title, salary, departmentId]);
-};
-
