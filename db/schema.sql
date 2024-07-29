cf7f2e05ba717728053a07d57cb4197a7772bb32
Ashley Wright <ashleywright@iMac.local>
2024-07-29 14:42:01 -0400

    finished creating the tables in the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,20 @@ CREATE DATABASE employee_db;
 
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL REFERENCES roles(id),
+    manager_id INTEGER REFERENCES employees(id)
+);
