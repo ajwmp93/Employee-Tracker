53276069bd33597418af975b142cb2c71e5d490b
Ashley Wright <ashleywright@iMac.local>
2024-07-29 16:19:24 -0400

    started working on the inquirer prompts for this tracker

---
+++ server.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+const {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
+} = require('./config/queries.js');
+
